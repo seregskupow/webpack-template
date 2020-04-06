f4614e7f67f53a659486053bd8c81bc256fe65ef
Sergey Skupow <sergskypow@gmail.com>
2020-04-07 02:06:39 +0300

    07.04.2020

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+import _ from "lodash";
+import "../styles/style.css";
+import Icon from "../assets/webpack.jpg";
+
+function component() {
+  const element = document.createElement("div");
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/js/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:"./src/index.html",
+      title: "Template",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, "src/js"),
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+                "@babel/preset-env"
+              ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 100,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
